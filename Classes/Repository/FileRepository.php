fe97706887336ce90177643e0e0be1622e72bd31
Benni Mack <benni@typo3.org>
2026-01-14 20:29:38 +0100

    [TASK] Add TYPO3 v14 support

--- Classes/Repository/FileRepository.php
+++ Classes/Repository/FileRepository.php
@@ -22,12 +22,14 @@ use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Resource\FileInterface;
 use TYPO3\CMS\Core\Resource\ProcessedFileRepository;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
+use TYPO3\CMS\Core\Resource\StorageRepository;
 
 class FileRepository
 {
     public function __construct(
         protected readonly ConnectionPool $connectionPool,
         protected readonly ProcessedFileRepository $processedFileRepository,
+        protected readonly StorageRepository $storageRepository,
         protected readonly ResourceFactory $resourceFactory
     ) {
     }
@@ -105,7 +107,8 @@ class FileRepository
         $rows = $this->findByIdentifier($identifier, $storage);
         foreach ($rows as $row) {
             try {
-                $file = $this->resourceFactory->getFileObjectByStorageAndIdentifier($row['storage'], $row['identifier']);
+                $storage = $this->storageRepository->getStorageObject($row['storage']);
+                $file = $storage->getFileByIdentifier($row['identifier']);
                 if (!$file) {
                     continue;
                 }
@@ -130,3 +133,4 @@ class FileRepository
         return count($rows);
     }
 }
+
--- Classes/Resource/RemoteResourceCollection.php
+++ Classes/Resource/RemoteResourceCollection.php
@@ -29,6 +29,7 @@ use TYPO3\CMS\Core\Resource\ProcessedFile;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
 use TYPO3\CMS\Core\Resource\ResourceInterface;
 use TYPO3\CMS\Core\Resource\ResourceStorage;
+use TYPO3\CMS\Core\Resource\StorageRepository;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class RemoteResourceCollection implements LoggerAwareInterface
@@ -143,7 +144,7 @@ class RemoteResourceCollection implements LoggerAwareInterface
         if (!array_key_exists($filePath, static::$fileIdentifierCache)) {
             static::$fileIdentifierCache[$filePath] = null;
             $localPath = $filePath;
-            $storage = $this->resourceFactory->getStorageObject(0, [], $localPath);
+            $storage = GeneralUtility::makeInstance(StorageRepository::class)->getStorageObject(0, [], $localPath);
             if ($storage->getUid() !== 0) {
                 static::$fileIdentifierCache[$filePath] = $this->getFileObjectFromStorage($storage, $fileIdentifier);
             }
@@ -163,7 +164,7 @@ class RemoteResourceCollection implements LoggerAwareInterface
         if (!$storage->isWithinProcessingFolder($fileIdentifier)) {
             try {
                 $fileObject = $this->resourceFactory->getFileObjectByStorageAndIdentifier($storage->getUid(), $fileIdentifier);
-            } catch (\InvalidArgumentException) {
+            } catch (\Throwable) {
                 return null;
             }
         } else {
